dd0291a494b9dd07266deb473a7a9572c6d893c5
Thiago Vieira <thiagovieira.dg@gmail.com>
2022-01-31 18:17:07 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+for(var i=0; i<=3; i++){
+    document.querySelector(".lista-conversas").innerHTML += `<li>
+    <div class="bloco-lateral">
+        <img src="src/img/profile.png" class="imagem-pessoa">
+    </div>
+    <div class="bloco-conversa">
+        <b>Nome da Pessoa</b>
+        <p>Inicio da mensagem</p>
+    </div>
+    <div class="bloco-lateral">19:00</div>
+    </li>`;
+}
+
